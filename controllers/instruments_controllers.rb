c8d6956e553f91bcf9f97ca1a41640f9611d38eb
Tech-A29 <Tech-A29@Tech-A29s-Air.lan>
2017-11-16 19:21:38 +0000

    pages added

---
+++ controllers/instruments_controllers.rb
@@ -0,0 +1,59 @@
+class InstrumentController < Sinatra::Base
+
+  # sets root as the parent directory of the current file
+  set :root, File.join(File.dirname(__FILE__), "..")
+  set :views, Proc.new{File.join(root, "views")}
+
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  $instruments = [{
+    id: 1,
+    name: "Piano",
+    info: "This is a string instrument",
+    image: "http://www.belfastmusic.co.uk/ekmps/shops/belfastmusic/images/v-piano-grand-1424-p.jpg"
+  },
+  {
+    id: 2,
+    name: "Trumpet",
+    info: "This is a brass instrument",
+    image: "https://images-na.ssl-images-amazon.com/images/I/41c90VJBDBL._SL500_AC_SS350_.jpg"
+  },
+  {
+    id: 3,
+    name: "Flute",
+    info: "This is a wind instrument",
+    image: "http://az632705.vo.msecnd.net/cmsroot/jupitermusic/media/jupiter-media/products/woodwinds/jaf1000e/angle1/jaf1000e_1.jpg"
+  }]
+
+  get "/" do
+    @title = "Instuments Homepage"
+    erb :"instruments/home"
+  end
+
+  get "/instruments" do
+    @title = "Instruments"
+    @instruments = $instruments
+    erb :"instruments/index"
+  end
+
+  get "instruments/new" do
+    @title = "New Instrument"
+    erb :"instruments/new"
+  end
+
+  get "/instruments/:name" do
+    id = params[:id].to_i
+    @instrument = $instruments[id]
+    @title = "Edit Instrument"
+    erb :"instruments/show"
+  end
+
+  get "/instruments/:name/edit" do
+    id = params[:id].to_i
+    @instrument = $instruments[id]
+    @title = "Edit instrument"
+    erb :"instruments/edit"
+  end
+end
